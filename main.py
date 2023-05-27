c40833e772e48061d15494126477f8b4eda4d7f7
KareemEzzat77 <kareemmowad83@gmail.com>
2023-05-27 03:33:12 +0100

    new

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel 
+import pickle
+import pandas as pd
+import model
+
+app = FastAPI()
+
+class item(BaseModel):
+    name:str
+    calories:int
+    fat:float
+    protein:float
+    iron:float
+    calcium:float
+    sodium:float
+    potassm :float
+    carbohydrate:float
+    fiber:float
+    vitamin_d:float
+    sugars:float
+with open("FinalPickle.pickle" , "rb") as file:
+    modelfile = pickle.load(file)
+
+def CompleteInputDummies(AllDataDummiesColumns , inputDummiesColumns):
+    Dummeies = []
+    
+    for i in AllDataDummiesColumns:
+        state=False
+        for x in inputDummiesColumns:
+           if i==x:
+               state=True
+        if state==True:
+            Dummeies.append(1)
+        else:
+            Dummeies.append(0)
+    return Dummeies                    
+@app.post("/")    
+def App(item:item):
+    
+    nameData = pd.Series(item.name)
+    nameDummies = pd.Series(nameData).str.get_dummies(sep=",")
+
+    AllDataDummies = model.namesDataFrameFinal
+    
+    Ou=CompleteInputDummies(AllDataDummies , nameDummies)
+    TextDummies = pd.DataFrame([Ou] ,columns=list(model.namesDataFrameFinal.columns) )
+    values = pd.DataFrame([{"calories":item.calories ,"total_fat" :item.fat ,"protein":item.protein ,"iron":item.iron  ,"calcium" :item.calcium , "sodium":item.sodium , "potassm":item.potassm , "carbohydrate":item.carbohydrate , "fiber":item.fiber, "vitamin_d":item.vitamin_d , "sugars":item.sugars}])
+    X_input = pd.concat([values , TextDummies] , axis=1)
+    distances, indices = modelfile.kneighbors(X_input)
+    print(type(indices))
+   
+
+    return {
+        "name":[model.data.iloc[indices.item(0),0]],
+        "name2":[model.data.iloc[indices.item(1),0]],
+        "name3":[model.data.iloc[indices.item(2),0]]
+    }
+
